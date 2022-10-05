d27c29b73e0865a36314b6c2913d5f0882612c4c
josesanchez2004 <jsanc618@mtroyal.ca>
2022-10-04 23:51:00 -0600

    edited toString() in Card class so that itwill print jack,queen,and king

--- Assignment2-2503-JoseSanchez-GabrielToro/src/mru/assignment2/comp2503/model/Card.java
+++ Assignment2-2503-JoseSanchez-GabrielToro/src/mru/assignment2/comp2503/model/Card.java
@@ -30,6 +30,12 @@ public class Card {
 	public String toString() {
 		if(getRank()==1) {
 			return "Ace of "+suit;
+		}else if (getRank()==11) {
+			return "Jack of "+suit;
+		}else if (getRank()==12) {
+			return "Queen of "+suit;
+		}else if (getRank()==13) {
+			return "King of "+suit;
 		}
 		return rank+" of "+ suit ;
 	}
